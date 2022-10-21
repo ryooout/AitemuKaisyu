db5cf74503dc54f9c0db8e7df6ad903a363b6fef
ryooout <ryota01010104@gmail.com>
2022-10-21 20:03:06 +0900

    カウントダウンが始まると同時に生成するように変更

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -7,10 +7,8 @@ public class ItemSpawner : MonoBehaviour
     float interval = 2.0f;
     float time;
     [SerializeField]private GameObject itemPrefab = default;
-    Rigidbody2D rb;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,8 +17,12 @@ public class ItemSpawner : MonoBehaviour
         time += Time.deltaTime;
         if(time>interval)
         {
-            Instantiate(itemPrefab, transform.position , Quaternion.identity);
-            time = 0;
+            if (GameManager.instance.IsStarted)
+            {
+                Instantiate(itemPrefab, transform.position, Quaternion.identity);
+                time = 0;
+            }
         }
     }
+    
 }
