27aacc76d8a91ed1909b93d0cd956dff956e9094
ryooout <ryota01010104@gmail.com>
2022-10-22 11:46:53 +0900

    タイマーが0になったと同時にアイテムの生成を辞める処理を記述した

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -7,6 +7,8 @@ public class ItemSpawner : MonoBehaviour
     float interval = 2.0f;
     float time;
     [SerializeField]private GameObject itemPrefab = default;
+    private bool _isSpawn;
+    public bool IsSpawn { get => _isSpawn; set => _isSpawn = value; }
     void Start()
     {
     }
@@ -14,15 +16,20 @@ public class ItemSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        interval = Random.Range(2.0f, 7.0f);
         time += Time.deltaTime;
         if(time>interval)
         {
             if (GameManager.instance.IsStarted)
             {
+                _isSpawn = true;
                 Instantiate(itemPrefab, transform.position, Quaternion.identity);
                 time = 0;
             }
+            else if(!GameManager.instance.IsStarted)
+            {
+                _isSpawn = false;
+            }
         }
-    }
-    
+    }    
 }
