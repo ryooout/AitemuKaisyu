4a8f95425cd9d40d5d50bcdb3fe89e5fd4bc8df6
ryooout <ryota01010104@gmail.com>
2022-10-23 10:28:39 +0900

    稀に爆弾の生成を行わせるような仕組みを作成。

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -6,30 +6,44 @@ public class ItemSpawner : MonoBehaviour
 {
     float interval = 2.0f;
     float time;
-    [SerializeField]private GameObject itemPrefab = default;
+    float randomItemSpawn;
+    [SerializeField]private GameObject[]itemPrefab = default;
     private bool _isSpawn;
     public bool IsSpawn { get => _isSpawn; set => _isSpawn = value; }
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
     void Update()
     {
         interval = Random.Range(2.0f, 7.0f);
+        randomItemSpawn = Random.Range(0, 101);
         time += Time.deltaTime;
-        if(time>interval)
+        if (time > interval)
         {
             if (GameManager.instance.IsStarted)
             {
                 _isSpawn = true;
-                Instantiate(itemPrefab, transform.position, Quaternion.identity);
-                time = 0;
             }
-            else if(!GameManager.instance.IsStarted)
+            else if (!GameManager.instance.IsStarted)
             {
                 _isSpawn = false;
             }
+            if (_isSpawn)
+            {
+                Generate();
+            }
         }
-    }    
+    }
+    /// <summary>弾の生成</summary>
+    void Generate()
+    {         //80%の確率で通常アイテム発射
+            if (randomItemSpawn >= 20)
+            {
+                Instantiate(itemPrefab[0], transform.position, Quaternion.identity);
+                time = 0;
+            }
+            //20%の確率で爆弾発射
+            else
+            {
+                Instantiate(itemPrefab[1], transform.position, Quaternion.identity);
+                time = 0;
+            }
+    }
 }
