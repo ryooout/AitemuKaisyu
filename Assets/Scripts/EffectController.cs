980d6c539b27abaced44ebccf24bb5e08d8e19df
ryooout <ryota01010104@gmail.com>
2022-11-09 13:52:52 +0900

    爆発エフェクトの再生

---
+++ Assets/Scripts/EffectController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectController : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _particle;
+    void Start()
+    {
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")||collision.gameObject.CompareTag("Player"))
+        {
+            // パーティクルシステムのインスタンスを生成する。
+            ParticleSystem newParticle = Instantiate(_particle);
+            // パーティクルの発生場所をこのスクリプトをアタッチしているGameObjectの場所にする。
+            newParticle.transform.position = this.transform.position;
+            // パーティクルを発生させる。
+            newParticle.Play();
+            Destroy(newParticle.gameObject, 0.5f);
+        }
+    }
+}
