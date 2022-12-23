fca31014eef7a7576af2f7dea79e1b1dd3a89576
ryooout <ryota01010104@gmail.com>
2022-12-23 19:57:07 +0900

    プレイヤーのキャラの動きをAnimatorのStringToHashを用いてint型の数値で動くようにした。コードの添削も行った。

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,40 +5,58 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField,Header("プレイヤーのスピード")]private float _speed = 5;
-    Rigidbody2D rb;
-    bool _isMove = false;
+    Rigidbody2D _rb;
+    Animator _animator;
+    /// <summary>animatorのIdを渡す。</summary>
+    private static readonly int RunId = Animator.StringToHash("Run");
+
     /// <summary>スピードのプロパティ</summary>
     public float Speed { get => _speed; set => _speed = value; }
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        //_animator.SetFloat("Speed", 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         if(GameManager.instance.IsStarted)
-        { 
-            _isMove = true;
-        }
-        else
-        {
-            _isMove = false; 
-        }
-        if(_isMove)
         {
             CharacterMove();
         }
-        else if(!_isMove)
+        else
         { 
-            rb.velocity = Vector2.zero; 
+            _rb.velocity = Vector2.zero;
+            _animator.SetBool(RunId, false);
         }
     }
 
     void CharacterMove()
     {
         float x = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(x * _speed, rb.velocity.y);
+        _rb.velocity = new Vector2(x * _speed, _rb.velocity.y);
+        if(x!=0)
+        {
+            _animator.SetBool(RunId, true);
+            Vector2 lscale = gameObject.transform.localScale;
+            if ((lscale.x > 0 && x < 0)
+                || (lscale.x < 0 && x > 0))
+            {
+                lscale.x *= -1;
+                gameObject.transform.localScale = lscale;
+            }
+            if(transform.position.x<=-13.8f)
+            {
+                transform.position = new Vector2(13.8f, transform.position.y);
+            }
+            else if(transform.position.x>=13.8f)
+            {
+                transform.position = new Vector2(-13.8f, transform.position.y);
+            }
+        }
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
